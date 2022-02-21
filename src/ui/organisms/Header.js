c7af90c50c9a76b2c2c288c21e64c8c16582b993
ryan-didevar <abdidevar@gmail.com>
2022-02-21 16:50:21 +0330

    A little bug fixed

--- src/ui/organisms/Header.js
+++ src/ui/organisms/Header.js
@@ -4,18 +4,18 @@ import styled from "styled-components";
 import Logo from "../../images/icon.svg";
 const Header = () => {
   return (
-    <Container className="Header_Container" >
-      <Navbar.Brand style={{paddingLeft:0}} href="/">
+    <Container className="Header_Container">
+      <Navbar.Brand style={{ paddingLeft: 0 }} href="/auto-icon-website/">
         <Icon>
           <img src={Logo} alt="Brand Auto Icon" width={30} height={30} />
           <BrandText className="poppins"> Auto Icon</BrandText>
         </Icon>
       </Navbar.Brand>
       <NavItemContainer>
-        <Nav.Item className="poppins" href="/plugin">
+        <Nav.Item className="poppins" href="/auto-icon-website/plugin">
           Plugin
         </Nav.Item>
-        <Nav.Item className="poppins" href="/new-icons">
+        <Nav.Item className="poppins" href="/auto-icon-website/new-icons">
           New Icons
         </Nav.Item>
       </NavItemContainer>
